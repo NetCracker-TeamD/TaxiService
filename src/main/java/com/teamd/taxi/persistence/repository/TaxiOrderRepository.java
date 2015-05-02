5a79a38340f801823e8f92eb67e8f3bed45c4267
Іван <ivanyv.ivan@yandex.ua>
2015-05-03 00:16:21 +0300

    Upadate

---
+++ src/main/java/com/teamd/taxi/persistence/repository/TaxiOrderRepository.java
@@ -0,0 +1,13 @@
+package com.teamd.taxi.persistence.repository;
+
+import com.teamd.taxi.entity.TaxiOrder;
+import com.teamd.taxi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface TaxiOrderRepository extends PagingAndSortingRepository<TaxiOrder, Long> {
+
+
+}
