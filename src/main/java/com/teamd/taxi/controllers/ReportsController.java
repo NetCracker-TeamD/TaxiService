a6e0dad046aa67f898be8f11fe17211f294a355d
Vita <vita@Antons-MacBook-Pro-2.local>
2015-05-14 21:52:01 +0300

    statistic for admin

---
+++ src/main/java/com/teamd/taxi/controllers/ReportsController.java
@@ -0,0 +1,298 @@
+package com.teamd.taxi.controllers;
+
+import com.teamd.taxi.persistence.repository.ReportsRepository;
+import com.teamd.taxi.service.ReportResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Controller
+@RequestMapping("/statistic")
+public class ReportsController {
+
+    @Autowired
+    private ReportsRepository reportsRepository;
+
+    @RequestMapping(value = "/serviceProfitabilityByMonth")
+    @ResponseBody
+    public List<Map<String, Object>> generateServiceProfitabilityReport() {
+        return reportsRepository.getReport(new ReportResolver() {
+            @Override
+            public String getQuery() {
+                return "SELECT " +
+                        "EXTRACT(MONTH FROM  route.start_time) as cur_month, " +
+                        "EXTRACT (YEAR FROM  route.start_time) as cur_year, sum(total_price) as total " +
+                        "FROM route " +
+                        "GROUP BY cur_month,cur_year  " +
+                        "ORDER BY cur_month desc";
+            }
+
+
+            @Override
+            public RowMapper getRowMapper() {
+                final String[] NAMES = {"January", "February", "March", "April", "May", "June", "July", "August",
+                        "September", "October", "November", "December"};
+                RowMapper<Map<String, Object>> mapper = new RowMapper<Map<String, Object>>() {
+                    @Override
+                    public Map<String, Object> mapRow(ResultSet resultSet, int i) throws SQLException {
+                        String year = resultSet.getString("cur_year");
+                        int month = resultSet.getInt("cur_month");
+                        String monthName = NAMES[month - 1];
+                        String total = resultSet.getString("total");
+                        Map<String, Object> result = new TreeMap<String, Object>();
+                        result.put("Period of time", monthName + ", " + year);
+                        result.put("Profit", total);
+                        return result;
+                    }
+                };
+                return mapper;
+            }
+
+            @Override
+            public Object[] getParams() {
+                return null;
+            }
+        });
+    }
+
+    @RequestMapping(value = "/newOrdersPerPeriod")
+    @ResponseBody
+    public List<Map<String, Object>> generateNewOrdersPerPeriodReport(@RequestParam("startDate") final String startDate,
+                                                                      @RequestParam("endDate")final String  endDate) {
+        final String from=startDate+" 00:00:00";
+        final String to=endDate+" 00:00:00";
+        return reportsRepository.getReport(new ReportResolver() {
+            @Override
+            public String getQuery() {
+                return "SELECT \"user\".last_name as user_last_name,\"user\".first_name as user_first_name," +
+                        "driver.last_name as driver_last_name,driver.first_name as driver_first_name," +
+                        "service_type.name as service_name,route.total_price as profit " +
+                        "FROM \"user\" " +
+                        "JOIN taxi_order ON taxi_order.user_id=\"user\".id " +
+                        "JOIN route ON route.order_id=taxi_order.id " +
+                        "JOIN driver ON driver.id=route.driver_id " +
+                        "JOIN service_type ON service_type.id=taxi_order.service_type " +
+                        "WHERE taxi_order.execution_date BETWEEN ? AND ?";
+            }
+
+            @Override
+            public RowMapper getRowMapper() {
+                return new RowMapper() {
+                    @Override
+                    public Map<String, Object> mapRow(ResultSet resultSet, int i) throws SQLException {
+                        String userLastName = resultSet.getString("user_last_name");
+                        String userFirstName = resultSet.getString("user_first_name");
+                        String driverLastName = resultSet.getString("driver_last_name");
+                        String driverFirstName = resultSet.getString("driver_first_name");
+                        String serviceTypeName = resultSet.getString("service_name");
+                        String totalPrice = resultSet.getString("profit");
+                        Map<String, Object> result = new TreeMap<String, Object>();
+                        result.put("User name", userFirstName + " " + userLastName);
+                        result.put("Driver name", driverFirstName + " " + driverLastName);
+                        result.put("Service name", serviceTypeName);
+                        result.put("Profit", totalPrice);
+                        return result;
+                    }
+                };
+            }
+            @Override
+            public Object[] getParams() {
+                return new Object[]{Timestamp.valueOf(from), Timestamp.valueOf(to)};
+
+            }
+        });
+    }
+
+    @RequestMapping(value = "/mostPopularAdditionalCarOptionsOverall")
+    @ResponseBody
+    public List<Map<String, Object>> generateMostPopularAdditionalCarOptionsOverallReport() {
+        return reportsRepository.getReport(new ReportResolver() {
+            @Override
+            public String getQuery() {
+                return "SELECT feature.name as feature_name,COUNT(order_features.feature_id) as usage " +
+                        "FROM order_features " +
+                        "JOIN feature ON order_features.feature_id=feature.id " +
+                        "GROUP BY feature.name " +
+                        "ORDER BY usage desc";
+            }
+
+            @Override
+            public RowMapper getRowMapper() {
+
+                return new RowMapper() {
+                    @Override
+                    public Map<String, Object> mapRow(ResultSet resultSet, int i) throws SQLException {
+                        String featureName = resultSet.getString("feature_name");
+                        String usage = resultSet.getString("usage");
+                        Map<String, Object> result = new TreeMap<String, Object>();
+                        result.put("Feature name", featureName);
+                        result.put("Count of usage", usage);
+                        return result;
+                    }
+                };
+            }
+
+            @Override
+            public Object[] getParams() {
+                return null;
+            }
+        });
+    }
+
+    @RequestMapping(value = "/mostPopularAdditionalCarOptionsForEachCustomerUser")
+    @ResponseBody
+    public List<Map<String, Object>> generateMostPopularAdditionalCarOptionsForEachCustomerUserReport() {
+        return reportsRepository.getReport(new ReportResolver() {
+            @Override
+            public String getQuery() {
+                return "SELECT title,max(feature_count)as max_count, user_name,user_surname " +
+                        "FROM " +
+                        "   (SELECT  feature.name as title,count(order_features.feature_id) AS feature_count," +
+                        "   \"user\".first_name as user_name,\"user\".last_name as user_surname " +
+                        "   FROM order_features  " +
+                        "   JOIN feature ON order_features.feature_id=feature.id  " +
+                        "   JOIN taxi_order ON order_features.order_id=taxi_order.id " +
+                        "   JOIN \"user\" ON \"user\".id=taxi_order.user_id " +
+                        "   GROUP BY \"user\".first_name,\"user\".last_name, feature.name " +
+                        "   ORDER by \"user\".first_name,\"user\".last_name" +
+                        "   )as temporary_table " +
+                        "GROUP BY user_name,user_surname,title " +
+                        "ORDER BY max_count desc";
+            }
+
+            @Override
+            public RowMapper getRowMapper() {
+                return new RowMapper() {
+                    @Override
+                    public Map<String, Object> mapRow(ResultSet resultSet, int i) throws SQLException {
+                        String title = resultSet.getString("title");
+                        String featureCount = resultSet.getString("max_count");
+                        String userName = resultSet.getString("user_name");
+                        String userSurname = resultSet.getString("user_surname");
+                        Map<String, Object> result = new TreeMap<String, Object>();
+                        result.put("Feature name", title);
+                        result.put("Count of usage", featureCount);
+                        result.put("User name", userName + " " + userSurname);
+                        return result;
+                    }
+                };
+            }
+
+            @Override
+            public Object[] getParams() {
+                return null;
+            }
+        });
+    }
+
+    @RequestMapping(value = "/mostPopularCar")
+    @ResponseBody
+    public List<Map<String, Object>> generateMostPopularCarReport() {
+
+        return reportsRepository.getReport(new ReportResolver() {
+            @Override
+            public String getQuery() {
+                return "SELECT car.model, car_class.class_name,COUNT(taxi_order.id) AS usage " +
+                        "FROM car  " +
+                        "JOIN car_class ON car.car_class_id=car_class.id " +
+                        "JOIN driver ON driver.id=car.driver_id " +
+                        "JOIN route ON driver.id=route.driver_id " +
+                        "JOIN taxi_order ON route.order_id=taxi_order.id " +
+                        "GROUP BY car.model, car_class.class_name " +
+                        "ORDER BY usage DESC";
+            }
+
+            @Override
+            public RowMapper getRowMapper() {
+                RowMapper<Map<String, Object>> mapper = new RowMapper<Map<String, Object>>() {
+                    @Override
+                    public Map<String, Object> mapRow(ResultSet resultSet, int i) throws SQLException {
+                        String model = resultSet.getString("model");
+                        String carClass = resultSet.getString("class_name");
+                        String usage = resultSet.getString("usage");
+                        Map<String, Object> result = new TreeMap<String, Object>();
+                        result.put("Car model", model);
+                        result.put("Car class", carClass);
+                        result.put("Count of usage", usage);
+                        return result;
+                    }
+                };
+                return mapper;
+            }
+
+            @Override
+            public Object[] getParams() {
+                return null;
+            }
+        });
+    }
+
+    @RequestMapping(value = "/mostProfitableService")
+    @ResponseBody
+    public List<Map<String, Object>> generateMostProfitableServiceReport(@RequestParam("period") final String period) {
+        return reportsRepository.getReport(new ReportResolver() {
+            @Override
+            public String getQuery() {
+                return "SELECT  service_type.name as service_name ,SUM(route.total_price) as profit " +
+                        "FROM service_type " +
+                        "JOIN taxi_order ON service_type.id=taxi_order.service_type " +
+                        "JOIN route ON taxi_order.id=route.order_id " +
+                        "WHERE taxi_order.execution_date BETWEEN ? AND ?  " +
+                        "GROUP BY service_type.name " +
+                        "ORDER BY profit desc";
+            }
+            @Override
+            public RowMapper getRowMapper() {
+                RowMapper<Map<String, Object>> mapper = new RowMapper<Map<String, Object>>() {
+                    @Override
+                    public Map<String, Object> mapRow(ResultSet resultSet, int i) throws SQLException {
+                        String serviceType = resultSet.getString("service_name");
+                        String profit = resultSet.getString("profit");
+                        Map<String, Object> result = new TreeMap<String, Object>();
+                        result.put("Service name", serviceType);
+                        result.put("Profit", profit);
+                        LocalDateTime endDate = LocalDateTime.now();
+                        LocalDateTime startDate=findEndDate(period);
+                        result.put("Period of time", startDate.toLocalDate().toString()+" - "+endDate.toLocalDate().toString());
+                        return result;
+                    }
+                };
+                return mapper;
+            }
+            @Override
+            public Object[] getParams() {
+                LocalDateTime endDate = LocalDateTime.now();
+                LocalDateTime startDate=findEndDate(period);
+                return new Object[]{Timestamp.valueOf(startDate), Timestamp.valueOf(endDate)};
+            }
+        });
+    }
+
+    private LocalDateTime findEndDate(final String  period){
+        LocalDateTime startDate;
+        if (period.equals("MONTH")) {
+            startDate = LocalDateTime.now().minusMonths(1);
+        } else if (period.equals("DECADE")) {
+            LocalDateTime half = LocalDateTime.now().minusMonths(2);
+            startDate = half.minusWeeks(2);
+        } else {
+            startDate = LocalDateTime.now().minusWeeks(1);
+        }
+        return startDate;
+    }
+
+    @RequestMapping
+    public ModelAndView viewStatistic(Model model, HttpServletRequest request) {
+        return new ModelAndView("statistic");
+    }
+}
--- src/main/webapp/WEB-INF/pages/resources/project/js/admin/statistic.js
+++ src/main/webapp/WEB-INF/pages/resources/project/js/admin/statistic.js
@@ -2,173 +2,191 @@
  * Created by Vika on 4/29/15.
  */
 
-$(function() {
-    $( "#datepicker" ).datepicker();
+
+var period;
+
+//время from to
+$(function () {
+    var pickerOpts = {
+        dateFormat: $.datepicker.ATOM
+    };
+    $("#datepicker").datepicker(pickerOpts);
+    $(function () {
+        $("#datepicker1").datepicker(pickerOpts);
+    });
 });
-$(function() {
-    $( "#datepicker1" ).datepicker();
-});
+
+
 $(document).ready(function () {
-        $('ul#my-menu ul').each(function (index) {
-            $(this).prev().addClass('collapsible').click(function () {
-                if ($(this).next().css('display') == 'none') {
-                    $(this).next().slideDown(200, function () {
-                        $(this).prev().removeClass('collapsed').addClass('expanded');
+    //выпадающий спикок все типов отчетов
+    $('ul#my-menu ul').each(function (index) {
+        $(this).prev().addClass('collapsible').click(function () {
+            if ($(this).next().css('display') == 'none') {
+                $(this).next().slideDown(200, function () {
+                    $(this).prev().removeClass('collapsed').addClass('expanded');
+                });
+            } else {
+                $(this).next().slideUp(200, function () {
+                    $(this).prev().removeClass('expanded').addClass('collapsed');
+                    $(this).find('ul').each(function () {
+                        $(this).hide().prev().removeClass('expanded').addClass('collapsed');
                     });
-                } else {
-                    $(this).next().slideUp(200, function () {
-                        $(this).prev().removeClass('expanded').addClass('collapsed');
-                        $(this).find('ul').each(function () {
-                            $(this).hide().prev().removeClass('expanded').addClass('collapsed');
-                        });
-                    });
-                }
-                return false;
-            });
+                });
+            }
+            return false;
+        });
+    });
+
+    //изменение параметра перод по изменинию checkbox
+    $('.checkboxes').change(function () {
+        findProfitByPeriod();
+    });
+
+    //Most profitable service
+    $(".profit_by_period").click(function () {
+        $('ul#my-menu li ul').hide("normal");
+        $("#report_1").show();
+        $("#report_5").hide();
+        $("#report_2").hide();
+        $("#report_3").hide();
+        $("#report_4").hide();
+        $("#report_6").hide();
+        findProfitByPeriod();
+    });
+
+    //нахождение параметра периода
+
+    function findProfitByPeriod() {
+        if ($("#decade").is(":checked")) {
+            period = "DECADE";
+        } else if ($("#month").is(":checked")) {
+            period = "MONTH";
+        } else {
+            period = "WEEK";
+        }
+        var parameterName = {
+            "period": period
+        };
+        var recursiveDecoded = decodeURIComponent($.param(parameterName));
+        var mapping = "statistic/" + "mostProfitableService?" + recursiveDecoded;
+        $.get(mapping, function (data) {
+            drawTable(data);
+        });
+    }
+
+
+    function drawTable(data) {
+        $('#main_table_content').empty();
+        var row_ap = $("<thead> <tr>");
+        $("#main_table_content").append(row_ap)
+        row_ap.append($("<th>" + "# " + "</th>"));
+        for (var prop in data[0]) {
+            row_ap.append($("<th>" + prop + "</th>"));
+        }
+        var row_down = $(" </tr> </thead> <tbody >");
+        $("#main_table_content").append(row_down)
+        for (var i = 0; i < data.length; i++) {
+            var row = $("<tr />");
+            $("#main_table_content").append(row);
+            row.append($("<td>" + (i + 1) + "</td>"));
+            for (var prop in data[i]) {
+                row.append($("<td>" + data[i][prop] + "</td>"));
+            }
+        }
+        var row_end = $("</tbody>");
+        $("#main_table_content").append(row_end);
+    }
+
+
+    //Most popular car
+    $(".popular_car").click(function () {
+        $('ul#my-menu li ul').hide("normal");
+        $("#report_1").hide();
+        $("#report_2").show();
+        $("#report_3").hide();
+        $("#report_4").hide();
+        $("#report_6").hide();
+        $("#report_5").hide();
+        $.get("statistic/mostPopularCar", function (data) {
+            drawTable(data);
+        });
+    });
+
+
+    //Most popular additional car options for each customer user
+    $(".option_for_user").click(function () {
+        $("#report_1").hide();
+        $("#report_2").hide();
+        $("#report_3").show();
+        $("#report_4").hide();
+        $("#report_6").hide();
+        $("#report_5").hide();
+        $('ul#my-menu li ul').hide("normal");
+        $.get("statistic/mostPopularAdditionalCarOptionsForEachCustomerUser", function (data) {
+            drawTable(data);
         });
 
-    $(".profit_by_period").click(function () {
-        $('ul#my-menu li ul').hide();
-        $("#pick_date").hide();
-        var cont='<div class="container">' +
-            ' <h2>Most profitable service</h2> ' ;
-        var content=
-            '<form class="container">' +
-            '<input type="radio" name="period" checked>Week</input><br>' +
-            '<input type="radio" name="period">Month</input><br>' +
-            '<input type="radio" name="period">Decade</input>' +
-            '</form> ' +
-            '<div class="table-responsive"> ' +
-            '<table class="table table-striped table-hover"> ' +
-            '<thead>' +
-            '<tr>' +
-            '<th>#</th> <th>Service name</th><th>Profit</th> <th>Period of time</th> ' +
-            '</tr>' +
-            '</thead>' +
-            '<tbody>' +
-            '<tr>' +
-            '<td>1</td><td>ASAP</td><td>1000</td><td>27 of April 2015 - 01 of May 2015</td>' +
-            '</tr>' +
-            '</tbody>' +
-            '</table> ' +
-            '</div>' +
-            '</div>';
-        $("#title").html(cont);
-        $("#main_content").html(content);
     });
-    $(".popular_car").click(function () {
-        $('ul#my-menu li ul').hide();
-        $("#pick_date").hide();
-        var cont='<div class="container">' +
-            ' <h2>Most popular car</h2> ' ;
-        var  content=
-            '<div class="table-responsive"> ' +
-            '<table class="table table-striped table-hover"> ' +
-            '<thead> ' +
-            '<tr> <th>#</th> <th>Car model</th> <th>Class</th> <th>Count of usage</th>' +
-            '</tr> ' +
-            '</thead>' +
-            '<tbody>' +
-            '<tr><td>1</td><td>DEO Lanos</td><td>Economy</td><td>15</td>' +
-            '</tr>' +
-            '</tbody>' +
-            '</table>' +
-            '</div>' +
-            '</div>';
-        $("#title").html(cont);
-        $("#main_content").html(content);
-    });
-    $(".option_for_user").click(function () {
-        $('ul#my-menu li ul').hide();
-        $("#pick_date").hide();
-        var cont='<div class="container">' +
-            '<h2>Most popular additional car options for each customer user</h2>';
-        var content=
-            '<div class="table-responsive">' +
-            '<table class="table table-striped table-hover">' +
-            '<thead>' +
-            '<tr>' +
-            '<th>#</th> <th>Option name</th><th>Customer name</th> <th>Count of usage</th>' +
-            '</tr>' +
-            '</thead>' +
-            '<tbody>' +
-            '<tr>' +
-            '<td>1</td><td>Conditioner</td><td>Ivanov Ivan</td><td>7</td>' +
-            '</tr>' +
-            '</tbody>' +
-            '</table>' +
-            '</div>' +
-            '</div>';
-        $("#title").html(cont);
-        $("#main_content").html(content);
-    });
+
+    ////Most popular additional car options
     $(".options").click(function () {
-        $('ul#my-menu li ul').hide();
-        $("#pick_date").hide();
-        var cont='<div class="container">' +
-            '<h2>Most popular additional car options overall</h2>';
-        var  content=
-            '<div class="table-responsive">' +
-            '<table class="table table-striped table-hover">' +
-            '<thead>' +
-            '<tr>' +
-            '<th>#</th> <th>Option name</th> <th>Count of usage</th>' +
-            '</tr>' +
-            '</thead>' +
-            '<tbody>' +
-            '<tr>' +
-            '<td>1</td><td>Wi-fi</td><td>17</td>' +
-            '</tr>' +
-            '</tbody>' +
-            '</table>' +
-            '</div>' +
-            '</div>';
-        $("#title").html(cont);
-        $("#main_content").html(content);
+        $('ul#my-menu li ul').hide("normal");
+        $("#report_1").hide();
+        $("#report_5").hide();
+        $("#report_2").hide();
+        $("#report_3").hide();
+        $("#report_4").show();
+        $("#report_6").hide();
+        $.get("statistic/mostPopularAdditionalCarOptionsOverall", function (data) {
+            drawTable(data);
+        });
     });
+
+    //New orders per period show table
     $(".new_order").click(function () {
-        $('ul#my-menu li ul').hide();
-        var content='<div class="container"> '+'<h2>New orders per period</h2> '
-          ;
-        $("#pick_date").show();
-        var  content2='<div class="table-responsive"> '+
-            '<table class="table table-striped table-hover">' +
-            '<thead>' +
-            '<tr> <th>#</th> <th>Customer name</th><th>Driver name</th> <th>Price</th><th>Service name</th> ' +
-            '</tr>' +
-            '</thead>' +
-            '<tbody><tr><td>1</td><td>Petrov Petr</td><td>Ivanov Ivan</td><td>73</td><td>ASAP</td>' +
-            '</tr>' +
-            '</tbody>' +
-            '</table>' +
-            '</div>' +
-            '</div>';
-        $("#title").html(content);
-        $("#main_content").html(content2);
+        $('#main_table_content').empty();
+        $('ul#my-menu li ul').hide("normal");
+        $("#report_5").show();
+        $("#report_1").hide();
+        $("#report_2").hide();
+        $("#report_3").hide();
+        $("#report_4").hide();
+        $("#report_6").hide();
     });
+    //ask for json from server with params
+    $("#generate").click(function () {
+        if ($("#datepicker").datepicker('getDate') != null & $("#datepicker1").datepicker('getDate') != null) {
+            var from = $('#datepicker1').datepicker({dateFormat: $.datepicker.ATOM}).val();
+            var to = $('#datepicker').datepicker({dateFormat: $.datepicker.ATOM}).val();
+            var parameterName = {
+                "startDate": from,
+                "endDate": to
+            };
+            var recursiveDecoded = decodeURIComponent($.param(parameterName));
+            var mapping = "statistic/" + "newOrdersPerPeriod" + "?" + recursiveDecoded;
+            $.get(mapping, function (data) {
+                drawTable(data);
+            });
+        } else {
+            alert("Please pick period of time");
+        }
+
+    });
+
+    //Service profitability by month
     $(".month_profit").click(function () {
-        $('ul#my-menu li ul').hide();
-        $("#pick_date").hide();
-        var cont='<div class="container">' +
-            '<h2>Service profitability by month</h2> ';
-        var content=
-            '<div class="table-responsive"> ' +
-            '<table class="table table-striped table-hover"> ' +
-            '<thead> ' +
-            '<tr> ' +
-            '<th>#</th> <th>Profit</th><th>Date</th> ' +
-            '</tr> ' +
-            '</thead> ' +
-            '<tbody>' +
-            '<tr>' +
-            '<td>1</td><td>1000</td><td>April, 2015</td>' +
-            '</tr>' +
-            '</tbody>' +
-            '</table>' +
-            '</div> ' +
-            '</div>';
-        $("#title").html(cont);
-        $("#main_content").html(content);
+        $('ul#my-menu li ul').hide("normal");
+        $("#report_1").hide();
+        $("#report_5").hide();
+        $("#report_2").hide();
+        $("#report_3").hide();
+        $("#report_4").hide();
+        $("#report_6").show();
+        $.get("statistic/serviceProfitabilityByMonth", function (data) {
+            drawTable(data);
+        });
+
     });
+
 
 });
